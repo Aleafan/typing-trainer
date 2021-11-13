769e52a606cfe87e7137972317ede969f0091990
Aleafan <ae.afanasov@yandex.com>
2021-11-13 09:32:17 +0300

    Добавил иконку farobot, обернул контент в div

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
 
 function Text(props) {
+  const {text} = props;
   return (
-    <p>
-      {props.text
-        ? props.text.split('').map((symbol, i) => <span key={i} id={i}>{symbol}</span>)
-        : 'Приготовьтесь, загружается текст...'}
-    </p>
+    <div className={text ? 'text' : 'text mes-prepare'}>
+      <p>
+        {text
+          ? text.split('').map((symbol, i) => <span key={i} id={i}>{symbol}</span>)
+          : 'Приготовьтесь, загружается текст...'}
+      </p>
+      {!text && 
+        <FontAwesomeIcon icon={faRobot} />}
+    </div>
   );
 }
 
