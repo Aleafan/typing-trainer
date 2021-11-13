79754910260003c258e7bfe139dbbf10f2419964
Aleafan <ae.afanasov@yandex.com>
2021-11-13 14:59:11 +0300

    Импортировал и добавил компонент Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Trainer from './components/Trainer';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Trainer />
+      <Footer />
     </React.Fragment>
   );
 }
