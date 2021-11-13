504fda63c2ea89b9c1c1e89c411f8872ac547fcd
Aleafan <ae.afanasov@yandex.com>
2021-11-13 09:35:09 +0300

    Добавил иконку fontawesome, класс

--- src/components/BtnRestart.js
+++ src/components/BtnRestart.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
 
 const BtnRestart = React.forwardRef((props, ref) => {
   return (
-    <button ref={ref} type='button' onClick={props.handleRestart}>
+    <button ref={ref} className='btn-restart' type='button' onClick={props.handleRestart}>
+      <FontAwesomeIcon icon={faRedoAlt} />
       ЗАНОВО
     </button>
   );
