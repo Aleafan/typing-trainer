3fd130ff6ddd0cd6a6df0cf7539506a2423b9e0b
Aleafan <ae.afanasov@yandex.com>
2021-11-13 11:38:49 +0300

    Написал логику подведения результата.

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import Speed from './Speed';
+import Precision from './Precision';
+
 function Result(props) {
+  const { speed, current, errors } = props;
+
+  let record = JSON.parse(localStorage.getItem('record'));
+  let newRecord = false;
+  if (!record || record.speed <= speed) {
+    newRecord = true;
+    record = { speed, current, errors };
+    localStorage.setItem('record', JSON.stringify(record));
+  }
+
   return (
-    <div className='text'>
-      <p>Поздравляем, хороший результат!</p>
+    <div className='text result'>
+      {newRecord
+        ? <p>
+            <FontAwesomeIcon icon={faTrophy} />
+            Поздравляем, это новый рекорд!
+          </p>
+        : <p>Поздравляем, хороший результат!</p>}
+      <div className='record'>
+        <p>
+        <FontAwesomeIcon icon={faTrophy} />
+          Текущий рекорд:
+        </p>
+        <div className='performance'>
+          <Speed speed={record.speed} />
+          <Precision current={record.current} errors={record.errors} />
+        </div>
+      </div>
     </div>
   );
 }
