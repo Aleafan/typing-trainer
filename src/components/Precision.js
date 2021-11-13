49fe7d93513c50f2fe440b961c854ea60f594ea1
Aleafan <ae.afanasov@yandex.com>
2021-11-13 09:37:00 +0300

    Добавил иконку fontawesome, обернул контент в div

--- src/components/Precision.js
+++ src/components/Precision.js
@@ -1,12 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCrosshairs } from '@fortawesome/free-solid-svg-icons';
+
 function Precision(props) {
   const { current, errors } = props;
   const precision = current
     ? Math.round(current * 100 / (current + errors))
     : 100;
   return (
-    <p>
-      Точность: <span>{precision}</span>%
-    </p>
+    <div className='badge'>
+      <p>
+        <FontAwesomeIcon icon={faCrosshairs} />
+        ТОЧНОСТЬ
+      </p>
+      <p><span>{precision}%</span></p>
+    </div>
   );
 }
 
